833ce5e509d4a94d6af5b0d8eaeb65c792ac8d08
Aruvansh Nigam <aruvanshn@gmail.com>
2018-07-14 02:05:58 +0530

    intial sync of progressbas and timer

--- BluetoothBatteryViewer/app/src/main/java/com/aruvansh/bluetoothbatteryviewer/BatteryCounter.java
+++ BluetoothBatteryViewer/app/src/main/java/com/aruvansh/bluetoothbatteryviewer/BatteryCounter.java
@@ -1,39 +1,67 @@
 package com.aruvansh.bluetoothbatteryviewer;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BatteryCounter extends AppCompatActivity {
 
-    String name, time = null;
+    String name,time = null;
     MyCountDownTimer myCountDownTimer;
     ProgressBar progressBar;
     Long timeleft;
     int progress;
+    SharedPreferences sharedPreferences;
+    public static final String mypreference = "mypref";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_battery_counter);
+       setContentView(R.layout.activity_battery_counter);
         Bundle bundle = getIntent().getExtras();
+        sharedPreferences = getSharedPreferences(mypreference,
+                Context.MODE_PRIVATE);
         time = bundle.getString("time");
         timeleft = Long.parseLong(time);
         name = bundle.getString("name");
-        TextView textView = (TextView) findViewById(R.id.textView);
+        progress=Integer.parseInt(bundle.getString("progress"));
+        TextView textView = (TextView) findViewById(R.id.DisplayText);
         progressBar=(ProgressBar)findViewById(R.id.progressBar);
-        textView.setText("The battery consumption by your device" + name);
+        Log.d("geuif uiefio ewifgew f", "onCreate: "+progress);
+        textView.setText("The battery consumption by your device " + name);
         ImageButton imageButton = findViewById(R.id.StartButton);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 progressBar.setProgress(progress);
-                myCountDownTimer = new MyCountDownTimer(timeleft * 3600000, 60000);
+                myCountDownTimer = new MyCountDownTimer(timeleft * 60000, 60000);
+                Toast.makeText(getApplicationContext(), "Your current pogree"+progress, Toast.LENGTH_LONG).show();
+                myCountDownTimer.intialProgress(progress);
                 myCountDownTimer.start();
             }
+        });
+        ImageButton imageButton1=findViewById(R.id.StopButton);
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                time=String.valueOf(progress/600);
+                Log.d("TAG", "onClicking stop button: "+progress);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(name, time+" "+progress);
+                Toast.makeText(getApplicationContext(), "Your time left"+time, Toast.LENGTH_LONG).show();
+                editor.commit();
+                myCountDownTimer.onPause();
+
+           }
         });
 
     }
@@ -47,8 +75,11 @@ public class BatteryCounter extends AppCompatActivity {
         @Override
         public void onTick(long millisUntilFinished) {
 
-            progress = (int) (millisUntilFinished / 100);
-            progressBar.setProgress(progress);
+            Log.d("hriofb ghreio gior", "onTick: "+millisUntilFinished);
+            progress = (int) (millisUntilFinished / 60000);
+
+            progressBar.setProgress(progressBar.getMax()-progress);
+
         }
 
         @Override
@@ -56,6 +87,16 @@ public class BatteryCounter extends AppCompatActivity {
 
             progressBar.setProgress(0);
         }
+        public void onPause() {
+            myCountDownTimer.cancel();
+
+        }
+        public void intialProgress(int progress)
+        {
+            progressBar.setProgress(progress);
+        }
 
     }
+
 }
+
--- BluetoothBatteryViewer/app/src/main/java/com/aruvansh/bluetoothbatteryviewer/Calculation.java
+++ BluetoothBatteryViewer/app/src/main/java/com/aruvansh/bluetoothbatteryviewer/Calculation.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -19,8 +20,9 @@ import android.widget.Toast;
 public class Calculation extends AppCompatActivity {
 
     String id,pos,name= null;
-    long time=0;
+    long time,progress;
     Object item;
+
     SharedPreferences sharedPreferences;
     public static final String mypreference = "mypref";
     int flag=1;
@@ -36,18 +38,24 @@ public class Calculation extends AppCompatActivity {
 
         final Animation animRotate = AnimationUtils.loadAnimation(this, R.anim.anim_rotate);
         Button save=(Button)findViewById(R.id.save);
+
         sharedPreferences = getSharedPreferences(mypreference,
                 Context.MODE_PRIVATE);
         if (sharedPreferences.contains(name)) {
-            time= Long.parseLong(sharedPreferences.getString(name," "));
+            String x=sharedPreferences.getString(name," ");
+           String temp[]=x.split(" ");
+            progress=Long.parseLong(temp[1]);
+            time= Long.parseLong(temp[0]);
+
             Toast.makeText(getApplicationContext(), "Already Stored "+ time ,Toast.LENGTH_SHORT).show();
-            flag=-1;
-            Intent intent=new Intent(Calculation.this, BatteryCounter.class);
-            Bundle bundle1=new Bundle();
-            bundle.putString("name",name);
-            bundle.putString("time", String.valueOf(time));
+            Intent intent=new Intent(this, BatteryCounter.class);
+             Bundle bundle1=new Bundle();
+            bundle1.putString("name",name);
+            bundle1.putString("progress",String.valueOf(progress));
+             bundle1.putString("time", String.valueOf(time));
             intent.putExtras(bundle1);
             startActivity(intent);
+
         }
         else {
             save.setOnClickListener(new Button.OnClickListener() {
@@ -59,26 +67,26 @@ public class Calculation extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putString(name,Long.toString(time));
+                    editor.putString(name, Long.toString(time)+" "+"100");
+                    Log.d("TAG", "onCreate:We have reached here "+time);
                     editor.commit();
                     v.startAnimation(animRotate);
-                    Toast.makeText(getApplicationContext(), "Your Preference has been Saved" ,Toast.LENGTH_LONG).show();
-                    TextView textView=(TextView)findViewById(R.id.displaymessage);
+                    Toast.makeText(getApplicationContext(), "Your Preference has been Saved", Toast.LENGTH_LONG).show();
+                    TextView textView = (TextView) findViewById(R.id.displaymessage);
                     textView.setText("Now you can go back and start with the calculation you can also reset the value of the claimed hours");
 
                 }
             });
 
-        }
-       TextView textView1=(TextView)findViewById(R.id.textView2);
-        textView1.setText("Please enter the claimed hours of battery time by your device "+ name);
-        Spinner spinner=(Spinner)findViewById(R.id.spinner);
-        String list[]={"1","2","3","4","5","6","7","8","9","10"};
-        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,list);
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(aa);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            TextView textView1 = (TextView) findViewById(R.id.textView2);
+            textView1.setText("Please enter the claimed hours of battery time by your device " + name);
+            Spinner spinner = (Spinner) findViewById(R.id.spinner);
+            String list[] = {"60", "120", "180", "240", "300", "360", "420", "480", "540", "600"};
+            ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_spinner_item, list);
+            aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(aa);
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
                     item = parent.getItemAtPosition(pos);
                     String stringToConvert = String.valueOf(item);
                     Long convertedLong = Long.parseLong(stringToConvert);
@@ -86,12 +94,14 @@ public class Calculation extends AppCompatActivity {
                     ((TextView) view).setTextColor(Color.RED);
 
                 }
-            public void onNothingSelected(AdapterView<?> parent) {
+
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
 
         }
-        });
-
-
 
     }
 
